6a0a2a0081601da8bf2d595e552a544ed7dcb994
dongglee <leedong1848@gamil.com>
2023-07-24 02:09:18 +0900

    6.2

--- src/main/scala/rng/RNG.scala
+++ src/main/scala/rng/RNG.scala
@@ -16,15 +16,45 @@ case class SimpleRNG(seed: Long) extends RNG {
 }
 
 object RNG {
+  // 6.1
   @tailrec
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (n, nextRng) = rng.nextInt
-    
+    if (n >= 0) (n, nextRng) else nonNegativeInt(nextRng)
   }
-}
 
-  @tailrec
-  def double(rng: RNG): (Double, RNG) = rng.nextDouble match {
-    case (n, next)
+  // 6.2
+  def double(rng: RNG): (Double, RNG) = {
+    val (n, nextRng) = RNG.nonNegativeInt(rng)
+    (n / (Int.MaxValue.toDouble + 1), nextRng)
+  }
+
+  // 6.3
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r1) = rng.nextInt
+    val (d, r2) = double(r1)
+    ((i, d), r2)
+  }
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, r1) = double(rng)
+    val (i, r2) = r1.nextInt
+    ((d, i), r2)
+  }
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
+
+  // 6.4
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count == 0)
+      (List.empty, rng)
+    else {
+      val (n, nextRng) = rng.nextInt
+      val (tail, retRng) = ints(count - 1)(nextRng)
+      (n :: tail, retRng)
+    }
   }
 }
--- src/test/scala/rng/SimpleRNGTest.scala
+++ src/test/scala/rng/SimpleRNGTest.scala
@@ -3,12 +3,27 @@ package rng
 import org.scalatest.funsuite.AnyFunSuiteLike
 
 class SimpleRNGTest extends AnyFunSuiteLike {
+  def rngGenerator[A](rng: RNG)(func: Function[RNG, (A, RNG)]): LazyList[A] = {
+    val (n, nextRng) = func(rng)
+    n #:: rngGenerator[A](nextRng)(func)
+  }
+
+  private def fixed = new {
+    def defaultRng: RNG = SimpleRNG(100)
+    def defaultGenerator[A]: (Function[RNG, (A, RNG)]) => LazyList[A] = rngGenerator(defaultRng)
+  }
+
   test("6.1 NegativeInt") {
-    var rng: RNG = SimpleRNG(100L);
-    for (_ <- 1 to 1000) {
-      RNG.nonNegativeInt(rng) match {
-        case (n, next) => assert(n > 0); rng = next; print(n)
-      }
-    }
+    fixed.defaultGenerator[Int](RNG.nonNegativeInt)
+      .map(e => assert(e >= 0)).take(1000).toList
+  }
+
+  test("6.2 double") {
+    fixed.defaultGenerator[Double](RNG.double)
+      .map(e => assert(0 <= e && e < 1)).take(100).toList
+  }
+
+  test("6.3 ints") {
+    assert(RNG.ints(100)(fixed.defaultRng)._1.length == 100)
   }
 }
