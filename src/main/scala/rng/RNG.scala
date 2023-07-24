6c0f93870b239095d5a4e260dbb16982f916ad76
dongglee <leedong1848@gamil.com>
2023-07-24 17:01:37 +0900

    6

--- src/main/scala/rng/RNG.scala
+++ src/main/scala/rng/RNG.scala
@@ -1,5 +1,7 @@
 package rng
 
+import rng.State.unit
+
 import scala.annotation.tailrec
 
 sealed trait RNG {
@@ -15,13 +17,50 @@ case class SimpleRNG(seed: Long) extends RNG {
   }
 }
 
+// 6.10
+case class State[S,+A](run: S => (A,S)) {
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    State(s => {
+      val (a, next) = run(s)
+      f(a).run(next)
+    })
+  def map[B](f: A => B): State[S, B] =
+      flatMap(e => unit(f(e)))
+  def map2[B, C](state: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => state.map(b => f(a, b)))
+}
+
+object State {
+  def unit[S,A](a: A): State[S,A] =
+    State(state => (a, state))
+  def sequence[S, A](states: List[State[S, A]]): State[S, List[A]] =
+    states.foldRight(unit[S, List[A]](List.empty))((v, acc) => v.map2(acc)(_ :: _))
+}
+
 object RNG {
+  private type State[S,+A] = S => (A, S)
+  type Rand[+A] = State[RNG, A]
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
   // 6.1
   @tailrec
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (n, nextRng) = rng.nextInt
     if (n >= 0) (n, nextRng) else nonNegativeInt(nextRng)
   }
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt)(i => i - i % 2)
+
+  def int: Rand[Int] =
+    _.nextInt
 
   // 6.2
   def double(rng: RNG): (Double, RNG) = {
@@ -57,4 +96,61 @@ object RNG {
       (n :: tail, retRng)
     }
   }
+
+  // 6.5
+  def doubleViaMap: Rand[Double] =
+    map(nonNegativeInt)(i => i / (Int.MaxValue.toDouble + 1))
+
+  // 6.6
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    (rng: RNG) => {
+      val (rav, rar) = ra(rng)
+      val (rbv, rbr) = rb(rar)
+      (f(rav, rbv), rbr)
+    }
+  }
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)((_, _))
+
+  def randIntDouble: Rand[(Int, Double)] =
+    both(int, double)
+
+  def randDoubleInt: Rand[(Double, Int)] =
+    both(double, int)
+
+  // 6.7
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List.empty[A]))((v, acc) => map2(v, acc)(_ :: _))
+
+  def intsViaSequence(count: Int)(rng: RNG): (List[Int], RNG) = {
+    sequence(List.fill(count)(int))(rng)
+  }
+
+  // 6.8
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, nextRng) = f(rng)
+      g(a)(nextRng)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt)(i => {
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    })
+
+  def mapViaFlatMap[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(e => unit(f(e)))
+
+  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => mapViaFlatMap(rb)(b => f(a, b)))
+
+  def bothViaFlatMap[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2ViaFlatMap(ra, rb)((_, _))
+
+  def randIntDoubleViaFlatMap: Rand[(Int, Double)] =
+    bothViaFlatMap(int, double)
+
+  def randDoubleIntViaFlatMap: Rand[(Double, Int)] =
+    bothViaFlatMap(double, int)
 }
--- src/test/scala/rng/SimpleRNGTest.scala
+++ src/test/scala/rng/SimpleRNGTest.scala
@@ -3,14 +3,14 @@ package rng
 import org.scalatest.funsuite.AnyFunSuiteLike
 
 class SimpleRNGTest extends AnyFunSuiteLike {
-  def rngGenerator[A](rng: RNG)(func: Function[RNG, (A, RNG)]): LazyList[A] = {
+  def rngGenerator[A](rng: RNG)(func: RNG.Rand[A]): LazyList[A] = {
     val (n, nextRng) = func(rng)
     n #:: rngGenerator[A](nextRng)(func)
   }
 
   private def fixed = new {
     def defaultRng: RNG = SimpleRNG(100)
-    def defaultGenerator[A]: (Function[RNG, (A, RNG)]) => LazyList[A] = rngGenerator(defaultRng)
+    def defaultGenerator[A]: (RNG.Rand[A]) => LazyList[A] = rngGenerator(defaultRng)
   }
 
   test("6.1 NegativeInt") {
@@ -25,5 +25,43 @@ class SimpleRNGTest extends AnyFunSuiteLike {
 
   test("6.3 ints") {
     assert(RNG.ints(100)(fixed.defaultRng)._1.length == 100)
+  }
+
+  test("6.5 doubleViaMap") {
+    fixed.defaultGenerator[Double](RNG.doubleViaMap)
+      .map(e => assert(0 <= e && e < 1)).take(100).toList
+  }
+
+  test("6.6 map2") {
+    val intDoubleStream = fixed.defaultGenerator[(Int, Double)](RNG.intDouble)
+    val randIntDoubleStream = fixed.defaultGenerator[(Int, Double)](RNG.randIntDouble)
+    val expr1 = intDoubleStream zip randIntDoubleStream map (e => assert(e._1 == e._2)) take 1000
+
+    val doubleIntStream = fixed.defaultGenerator[(Double, Int)](RNG.doubleInt)
+    val randDoubleIntStream = fixed.defaultGenerator[(Double, Int)](RNG.randDoubleInt)
+    val expr2 = doubleIntStream zip randDoubleIntStream map (e => assert(e._1 == e._2)) take 1000
+
+    (expr1.toList, expr2.toList)
+  }
+
+  test("6.7 ints") {
+    assert(RNG.intsViaSequence(1000)(fixed.defaultRng)._1.length == 1000)
+  }
+
+  test("6.8 nonNegativeLessThen") {
+    val targetStream = fixed.defaultGenerator(RNG.nonNegativeLessThan(6))
+    targetStream.map(e => assert(0 <= 0 && e < 6)).take(1000).toList
+  }
+
+  test("6.9 flatMap") {
+    val intDoubleStream = fixed.defaultGenerator[(Int, Double)](RNG.intDouble)
+    val randIntDoubleStream = fixed.defaultGenerator[(Int, Double)](RNG.randIntDoubleViaFlatMap)
+    val expr1 = intDoubleStream zip randIntDoubleStream map (e => assert(e._1 == e._2)) take 1000
+
+    val doubleIntStream = fixed.defaultGenerator[(Double, Int)](RNG.doubleInt)
+    val randDoubleIntStream = fixed.defaultGenerator[(Double, Int)](RNG.randDoubleIntViaFlatMap)
+    val expr2 = doubleIntStream zip randDoubleIntStream map (e => assert(e._1 == e._2)) take 1000
+
+    (expr1.toList, expr2.toList)
   }
 }
